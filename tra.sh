742663025343d32c75dbd9e3cf6e44b265bf16ee
hiruocha <hiruocha@gmail.com>
2025-11-09 00:40:51 +0800

    feat(ls): 初步实现 ls 功能
    - 简单地列出所有回收站内容，每行一个
    - 使用了 https://github.com/ko1nksm/url 中的 urldecode 函数来实现 url 解码

---
+++ tra.sh
@@ -0,0 +1,115 @@
+#!/bin/sh
+# Copyright © 2025 hiruocha
+
+# This program is free software: you can redistribute it and/or modify it under the 
+# terms of the GNU General Public License as published by the Free Software 
+# Foundation, either version 3 of the License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY 
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or 
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for 
+# more details.
+
+# You should have received a copy of the GNU General Public License along with this 
+# program. If not, see <https://www.gnu.org/licenses/>. 
+
+uid=$(id -ru)
+home_trash=${XDG_DATA_HOME:-$HOME/.local/share}/Trash
+
+cmd="${1:-help}"
+
+# https://github.com/ko1nksm/url
+urldecode() {
+  LC_ALL=C awk -v space="$SHURL_SPACE" -v eol="$SHURL_EOL" '
+    function decode(map, str,   ret) {
+      while (match(str, /%[0-9A-Fa-f][0-9A-Fa-f]/)) {
+        ret = ret substr(str, 1, RSTART - 1) url[substr(str, RSTART + 1, 2)]
+        str = substr(str, RSTART + RLENGTH)
+      }
+      return ret str
+    }
+
+    BEGIN {
+      for (i = 0; i < 256; i++) url[sprintf("%02x", i)] = sprintf("%c", i)
+
+      # Increase to 4 patterns to improve performance
+      for (k in url) {
+        m = substr(k, 1, 1); M = toupper(m)
+        l = substr(k, 2, 1); L = toupper(l)
+        url[m L] = url[M l] = url[M L] = url[k]
+      }
+    }
+
+    BEGIN {
+      for (i = 1; i < ARGC; i++) {
+        if (length(space) > 0) gsub(space, " ", ARGV[i])
+        print decode(url, ARGV[i])
+      }
+      if (ARGC > 1) exit
+    }
+
+    {
+      if (length(space) > 0) gsub(/\+/, " ", $0)
+      print decode(url, $0)
+    }
+  ' "$@"
+}
+
+list() {
+  [ -d "$trash" ] || return
+  for trashinfo in "$trash"/info/*.trashinfo
+  do
+    [ -e "$trashinfo" ] || continue
+    path=$(urldecode "$(awk -F '=' '/^Path=/ {print $2; exit}' "$trashinfo")")
+    printf '%s' "$path"
+    filename=${trashinfo##*/}
+    filename=${filename%.trashinfo}
+    if
+      [ ! -e "$trash"/files/"$filename" ]
+    then
+      printf ' [MISSING]\n'
+    elif
+      [ -d "$trash"/files/"$filename" ]
+    then
+      printf ' (dir)\n'
+    else
+      printf '\n'
+    fi
+  done
+}
+
+cmd_ls() {
+  {
+    df -P | tail -n +2 | while read -r fs; do
+      case "$fs" in
+        /dev/*)
+          topdir=$(printf '%s' "$fs" | awk '{print $NF}')
+          if
+            [ "$topdir" = "$(df -P "$home_trash" | awk 'NR==2 {print $NF}')" ]
+          then
+            trash="$home_trash"
+            list
+          elif
+            [ -d "$topdir/.Trash" ] &&
+            [ ! -L "$topdir/.Trash" ] &&
+            [ -n "$(find "$topdir/.Trash" -prune -type d -perm -1000)" ]
+          then
+            trash="$topdir"/.Trash/"$uid"
+            list
+          else
+            trash="$topdir"/.Trash-"$uid"
+            list
+          fi
+          ;;
+        *)
+          ;;
+      esac
+    done
+  } | sort
+}
+
+case "$cmd" in
+  ls)
+    cmd_ls
+    ;;
+esac
